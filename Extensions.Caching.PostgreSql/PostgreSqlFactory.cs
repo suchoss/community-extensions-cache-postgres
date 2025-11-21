d04127d8a730706d9a4b976f03bc787fcea63f41
Suchoss <suchoss@gmail.com>
2025-11-21 15:11:33 +0100

    Add factory method

---
+++ Extensions.Caching.PostgreSql/PostgreSqlFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+
+namespace Community.Microsoft.Extensions.Caching.PostgreSql
+{
+    public sealed class PostgreSqlFacory
+    {
+        public IDistributedCache Create(PostgreSqlCacheOptions options, ILoggerFactory loggerFactory)
+        {
+            IOptions<PostgreSqlCacheOptions> opt = Options.Create(options);
+            
+            ILogger<DatabaseOperations> dbopsLogger = loggerFactory.CreateLogger<DatabaseOperations>();
+            ILogger<DatabaseExpiredItemsRemoverLoop> dbremoverLoopLogger = loggerFactory.CreateLogger<DatabaseExpiredItemsRemoverLoop>();
+            
+            DatabaseOperations dbops = new DatabaseOperations(opt, dbopsLogger);
+            DatabaseExpiredItemsRemoverLoop dbloop = new DatabaseExpiredItemsRemoverLoop(opt, dbops, dbremoverLoopLogger);
+            
+            return new PostgreSqlCache(opt, dbops, dbloop);
+        }
+    }
+}
